024dfa9b4733ff62fe4a3bc7c3d7b6d4c7ae6d1b
ericivander <eric.ivander@bukalapak.com>
2020-07-30 22:08:46 +0700

    implement category repository interface

---
+++ repository/postgres/category.go
@@ -0,0 +1,39 @@
+package postgres
+
+import (
+	"github.com/ericivander/monrep/entity"
+	dbEntity "github.com/ericivander/monrep/repository/postgres/entity"
+	"github.com/jmoiron/sqlx"
+)
+
+// CategoryProvider holds database connection
+type CategoryProvider struct {
+	db *sqlx.DB
+}
+
+func (p *CategoryProvider) fetch(query string, args ...interface{}) ([]*entity.Category, error) {
+	rows, err := p.db.Queryx(query, args...)
+	if err != nil {
+		return nil, err
+	}
+
+	defer rows.Close()
+
+	result := make([]*entity.Category, 0)
+
+	for rows.Next() {
+		tmpCategory := dbEntity.Category{}
+		if err := rows.StructScan(&tmpCategory); err != nil {
+			return nil, err
+		}
+
+		result = append(result, tmpCategory.ToEntity())
+	}
+
+	return result, nil
+}
+
+// GetCategories return all categories on database
+func (p *CategoryProvider) GetCategories() ([]*entity.Category, error) {
+	return p.fetch("SELECT id, name, type FROM categories")
+}
---
+++ repository/postgres/entity/category.go
@@ -0,0 +1,22 @@
+package entity
+
+import (
+	"github.com/ericivander/monrep/entity"
+	"github.com/ericivander/monrep/entity/types"
+)
+
+// Category is struct for category on database
+type Category struct {
+	ID   int    `db:"id"`
+	Name string `db:"name"`
+	Type int16  `db:"type"`
+}
+
+// ToEntity convert database object to entity
+func (c *Category) ToEntity() *entity.Category {
+	return &entity.Category{
+		ID:   c.ID,
+		Name: c.Name,
+		Type: types.CategoryType(c.Type),
+	}
+}
